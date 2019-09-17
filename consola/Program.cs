156a5316dc1d458693c7b4069d7262e0d031a412
josealex <jose_delacruz4@usmp.pe>
2019-09-17 04:38:17 -0500

    primer programa

--- consola/Program.cs
+++ consola/Program.cs
@@ -6,7 +6,13 @@ namespace consola
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola como te llams ?");
+            string nombre =Console.ReadLine();
+            Console.WriteLine("que edad tienes");
+            var edad =Convert.ToInt32( Console.ReadLine());
+            var edadamigo=edad+5;
+            Console.WriteLine("Hello"+ nombre);
+            Console.WriteLine($"hello {nombre}");
         }
     }
 }
